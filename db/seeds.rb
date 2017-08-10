e2ba8b58ff9de57089d070377bbf81057325e1c9
Joaquin Grez <joaquingrez@gmail.com>
2017-08-09 22:27:54 -0400

    DB dumped to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-Genre.create(name: "Rock")
-Genre.create(name: "Hip Hop")
-Genre.create(name: "Techno")
-Genre.create(name: "Indie")
-Genre.create(name: "Psychodelic")
-Genre.create(name: "House")
-Genre.create(name: "Blues")
+User.create!([
+  {email: "joaquin@joaquin.cl", encrypted_password: "$2a$11$k/rnZjqciIcSPbatQMxMG.aGNoVFBvxCOc6Y4pgVcOlrdF.EmWs32", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: "2017-08-10 00:22:29", sign_in_count: 3, current_sign_in_at: "2017-08-10 00:22:29", last_sign_in_at: "2017-08-10 00:20:40", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", name: "Joaquin"}
+])
+Genre.create!([
+  {name: "Rock"},
+  {name: "Hip Hop"},
+  {name: "Techno"},
+  {name: "Indie"},
+  {name: "Psychodelic"},
+  {name: "House"},
+  {name: "Blues"}
+])
+Song.create!([
+  {name: "Hola", duration: 300, genre_id: 1},
+  {name: "Chao", duration: 200, genre_id: 4},
+  {name: "Tapa", duration: 345, genre_id: 3},
+  {name: "Lala", duration: 456, genre_id: 6},
+  {name: "Yoyo", duration: 243, genre_id: 4}
+])
+UserSong.create!([
+  {position: 1, user_id: 1, song_id: 1},
+  {position: 2, user_id: 1, song_id: 2},
+  {position: 3, user_id: 1, song_id: 4},
+  {position: 4, user_id: 1, song_id: 5}
+])
