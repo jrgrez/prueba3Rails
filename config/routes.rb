f6fe622d2ce89f4472f76fdb6c55596103b9c9fb
Joaquin Grez <joaquingrez@gmail.com>
2017-08-09 21:26:58 -0400

    Routes improved

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
         registrations: 'users/registrations'
       }
-
+  resources :users do
+  	resources :songs
+  end
   resources :songs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "songs#index"
